418ecc649aa76d45c060d337a3123b31068c8a81
AngryCarrot <ilmarvvv@gmail.com>
2025-12-30 20:45:48 +0100

    Improve comment

--- LINQMethods/Program.cs
+++ LINQMethods/Program.cs
@@ -5,6 +5,45 @@
         static void Main(string[] args)
         {
             // TODO: Demonstrate some LINQ methods here
+
+            /* Aggregate Operator */
+
+            // array.Sum();
+            // array.Min();
+            // array.Max();
+            // array.Average();
+            // array.Count();
+            // array.Aggregate();
+
+            /* Filtering Operator */
+            // array.Where();
+
+            /* Projection Operator */
+            // array.Select();
+
+            /* Sorting Operation */
+            // array.OrderBy();
+            // array.OrderByDescending();
+
+            /* Set Operators */
+            // array.Concat();
+            // array.Union();
+            // array.Intersect();
+            // array.Except();
+
+            /* Other */
+
+            // array.First();
+            // array.FirstOrDefault();
+            // array.Last();
+            // array.LastOrDefault();
+            // array.Single();
+            // array.SingleOrDefault();
+            // array.Any();
+            // array.All();
+
+
+
         }
     }
 }
