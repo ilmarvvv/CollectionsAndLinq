626a6d5f404d10369bf6afd6abb9996bf56d7e5a
AngryCarrot <ilmarvvv@gmail.com>
2026-01-10 12:10:46 +0100

    docs(array): add structured overview of Array properties

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Core
+            #region Length - gets the total number of elements in all dimensions of the array
+
+            #endregion
+
+            #region LongLength - gets the total number of elements in all dimensions of the array as a long
+
+            #endregion
+
+            #region Rank - gets the number of dimensions of the array
+
+            #endregion
+
+            // Limits
+
+            #region MaxLength - gets the maximum number of elements an array can contain
+
+            #endregion
+
+            // Collection / Threading
+
+            #region IsFixedSize - gets a value indicating whether the array has a fixed size
+
+            #endregion
+
+            #region IsReadOnly - gets a value indicating whether the array is read-only
+
+            #endregion
+
+            #region IsSynchronized - gets a value indicating whether access to the array is synchronized (thread safe)
+
+            #endregion
+
+            #region SyncRoot - gets an object that can be used to synchronize access to the array
+
+            #endregion
+        }
+    }
+}
